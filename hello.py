021111b5a9fef8c20466f409aa2c30c1bfbb1031
ryanjsfx2424 <ryanjsfx24@gmail.com>
2024-07-03 09:10:49 -0700

    added homework tasks and hello world program to verify VS code works

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("hello world")
+"""
+Homework:
+1. Watch Apollo 13 (within next week)
+2. Install VS Code
+3. First github repo with hello world (confirm VS Code installation is working w/ Python)
+4. Bart Simpson Program
+    Modify hello world to use a loop: to print w/e 1000 times. Show how many times each loop was done.
+5. Birthday Reminder Program
+6. Calculator Program
+
+7. Athena tutorials
+"""
